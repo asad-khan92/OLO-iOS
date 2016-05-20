edbf1a21217c0a679779806830040ea3cd661d2b
Asad Khan <asad.simple92@gmail.com>
2016-05-20 16:03:22 +0500

    initial comment

---
+++ OLOO/OLOO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OLOO
+//
+//  Created by kashif Saeed on 20/05/2016.
+//  Copyright © 2016 attribes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OLOO/OLOO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OLOO
+//
+//  Created by kashif Saeed on 20/05/2016.
+//  Copyright © 2016 attribes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
